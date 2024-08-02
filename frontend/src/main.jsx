84cbae2ff1174a8e4f3c2ae5b5a6072cb00ab11e
Pratishtha Kumari <pratishtha.richu@gmail.com>
2024-08-02 11:38:10 +0530

    deploy config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Router from "./Router.jsx";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Router from './Router.jsx'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router />
-    </Provider>
-  </React.StrictMode>
-);
+if (import.meta.env.VITE_NODE_ENV === 'production') disableReactDevTools()
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<Router />
+		</Provider>
+	</React.StrictMode>
+)
